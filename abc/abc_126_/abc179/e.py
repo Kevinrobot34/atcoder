4b9a39eb4874317bac46ee225a64138355124a7d
Kosaku Ono <kevinrobot34@yahoo.co.jp>
2020-09-30 17:00:38 +0900

    Solve a problem

--- abc/abc_126_/abc179/e.py
+++ abc/abc_126_/abc179/e.py
@@ -1,13 +1,20 @@
 n, x, m = map(int, input().split())
 
-a_l = [x]
-a_s = set(a_l)
+a_idx = [-1] * m
+a_idx[x] = 0
+a = [x]
+idx = 1
 while True:
-    y = (a_l[-1]**2) % m
-    if y in a_s:
+    y = (a[-1]**2) % m
+    if a_idx[y] != -1:
+        i = a_idx[y]
+        j = len(a) - i
         break
-    a_l.append(y)
-    a_s.add(y)
+    else:
+        a_idx[y] = idx
+        idx += 1
+        a.append(y)
 
-ans = sum(a_l) * (n // len(a_l)) + sum(a_l[:n % len(a_l)])
+ans = sum(a) * (n)
+ans = sum(a[:i]) + sum(a[i:]) * ((n - i) // j) + sum(a[i:i + (n - i) % j])
 print(ans)
