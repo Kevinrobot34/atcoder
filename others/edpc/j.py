f1efa01791d551ebaeeda9688e65e4461bbabd4f
Kosaku Ono <kevinrobot34@yahoo.co.jp>
2021-03-07 02:42:17 +0900

    Solve some problems

--- others/edpc/j.py
+++ others/edpc/j.py
@@ -1,26 +1,26 @@
-from collections import Counter
+import sys
+
+sys.setrecursionlimit(10**8)
 n = int(input())
 a = list(map(int, input().split()))
-cnt = Counter(a)
-memo = {}
+memo = [[[-1] * (n + 1) for _ in range(n + 1)] for _ in range(n + 1)]
 
 
 def f(i, j, k):
-    # f(i, j, k) = (さらに0,1,2個の寿司が置かれている皿がi,j,k枚ある場合の)
-    if (i, j, k) in memo:
-        return memo[(i, j, k)]
+    # f(i, j, k) = (さらに0,1,2個の寿司が置かれている皿がi,j,k枚ある場合から始めたときの、寿司がなくなるまでの操作回数の期待値)
+    if memo[i][j][k] != -1:
+        return memo[i][j][k]
     if i == n:
         return 0
     l = n - i - j - k
-    r1 = j * (f(i + 1, j - 1, k) + 1) if j > 0 else 0
-    r2 = k * (f(i, j + 1, k - 1) + 1) if k > 0 else 0
-    r3 = l * (f(i, j, k + 1) + 1) if l > 0 else 0
-    ret = (i / n) / ((1 - i / n)**2) * (r1 + r2 + r3) / n
+    r1 = j * f(i + 1, j - 1, k) / n if j > 0 else 0
+    r2 = k * f(i, j + 1, k - 1) / n if k > 0 else 0
+    r3 = l * f(i, j, k + 1) / n if l > 0 else 0
+    ret = (1 + r1 + r2 + r3) / (1 - i / n)
 
-    memo[(i, j, k)] = ret
+    memo[i][j][k] = ret
     return ret
 
 
-ans = f(0, cnt[1], cnt[2])
-print(memo)
+ans = f(0, a.count(1), a.count(2))
 print(ans)
---
+++ others/edpc/s.py
@@ -0,0 +1,36 @@
+MOD = 10**9 + 7
+base = 10
+k = input()
+d = int(input())
+n = len(k)
+dp1 = [[0] * d for _ in range(n + 1)]
+dp2 = [[0] * d for _ in range(n + 1)]
+# dp1[i][j] : 上からi桁はkと一致していて、そこまでの各桁の数字の和をdで割った余りがj
+# dp2[i][j] : 上からi桁が既にnより小さく、そこまでの各桁の数字の和をdで割った余りがj
+
+dp1[0][0] = 1
+x = 0
+for i in range(n):
+    ki = int(k[i])
+    # 1 -> 1
+    j_11 = (x + ki) % d
+    dp1[i + 1][j_11] += dp1[i][x]
+    dp1[i + 1][j_11] %= MOD
+
+    # 1 -> 2
+    for a in range(ki):
+        j_12 = (x + a) % d
+        dp2[i + 1][j_12] += dp1[i][x]
+        dp2[i + 1][j_12] %= MOD
+
+    # 2 -> 2
+    for y in range(d):
+        for a in range(base):
+            j_22 = (y + a) % d
+            dp2[i + 1][j_22] += dp2[i][y]
+            dp2[i + 1][j_22] %= MOD
+
+    x = (x + ki) % d
+
+ans = (dp1[n][0] + dp2[n][0] - 1) % MOD  # 0は取り除く
+print(ans)
